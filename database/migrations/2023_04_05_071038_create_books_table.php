c329bc00a466df92c59653fe6516289039e0ffdd
imranskylinx <imran.skylinxtech@gmail.com>
2023-04-07 12:46:27 +0500

    table added in book model

--- database/migrations/2023_04_05_071038_create_books_table.php
+++ database/migrations/2023_04_05_071038_create_books_table.php
@@ -18,7 +18,7 @@ class CreateBooksTable extends Migration
             $table->string('title');
             $table->string('description')->nullable();
             $table->string('file');
-            $table->string('thumbnail')->nullable();
+            $table->string('cover')->nullable();
             $table->string('tags');
             $table->unsignedBigInteger('added_by');
             $table->timestamps();
