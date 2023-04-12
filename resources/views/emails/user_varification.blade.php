4b5dad15deeb36f58bd025f74331762c050511a0
imranskylinx <imran.skylinxtech@gmail.com>
2023-04-12 11:43:16 +0500

    spell mistake corrected

--- resources/views/emails/user_varification.blade.php
+++ resources/views/emails/user_varification.blade.php
@@ -4,9 +4,9 @@
     @component('mail::button', [
         'url' => url("localhost:3000/verify-account?email=$user->email &token=$user->id"),
     ])
-        Varify You Email
+        Verify You Email
     @endcomponent
 
-    Thanks,<br>
+    Thanks
     {{ config('app.name') }}
 @endcomponent
