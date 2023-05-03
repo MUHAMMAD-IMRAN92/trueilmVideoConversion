37f6af6e24044f0558c16d0bc908611e00d14d9d
imranskylinx <imran.skylinxtech@gmail.com>
2023-05-03 19:29:56 +0500

    route added

--- resources/views/static_page.blade.php
+++ resources/views/static_page.blade.php
@@ -25,6 +25,7 @@
     }
 </style>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <body class="body-class">
