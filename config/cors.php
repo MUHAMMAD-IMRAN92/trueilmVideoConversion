56412ae3a908d0859444a59a272d4400e704173d
imranskylinx <imran.skylinxtech@gmail.com>
2023-05-15 18:33:52 +0500

    cors access changed

--- config/cors.php
+++ config/cors.php
@@ -19,11 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['app.trueilm.com/*'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => ['*'],
 
-    'allowed_headers' => ['app.trueilm.com/*'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
