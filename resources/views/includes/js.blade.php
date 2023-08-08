421124c8d988d14e788ea1aacba87eb93209d46f
imranskylinx <imran.skylinxtech@gmail.com>
2023-08-08 23:21:49 +0500

    d-flex

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -706,7 +706,7 @@
                                anchor =
                                    `<a  class="ml-2" target="_blank" href="{{ url('book/view/`+row._id+`') }}"><i class="fa fa-eye" style="font-size:24px"></i></a>`;
                            }
-                           return `<td>
+                           return `<td class="d-flex">
                                 <a  class="ml-2" href="{{ url('book/approve/`+row._id+`') }}"><i class="fa fa-check" style="font-size:24px"></i></a>
                                 <span class="ml-2"><i class="fa fa-times" onclick="reasonModal('${row._id}')" style="font-size:24px; cursor:pointer"  data-href=""></i></span>` +
                                anchor +
