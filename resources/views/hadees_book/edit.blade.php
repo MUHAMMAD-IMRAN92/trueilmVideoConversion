a068c54156cdbf11481c6144049d450a4688df73
imranskylinx <imran.skylinxtech@gmail.com>
2023-08-16 18:45:42 +0500

    css in edit Hadees

--- resources/views/hadees_book/edit.blade.php
+++ resources/views/hadees_book/edit.blade.php
@@ -91,7 +91,6 @@
                 <div class="content-header-left col-md-9 col-12 mb-2">
                     <div class="row breadcrumbs-top">
                         <div class="col-12">
-                            <h2 class="content-header-title float-left mb-0">Hadith Book</h2>
                             <div class="breadcrumb-wrapper col-12">
                             </div>
                         </div>
