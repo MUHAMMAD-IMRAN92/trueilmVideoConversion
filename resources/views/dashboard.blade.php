34cbe4b741691b75d7b0f1552d8cb8cebec88fd9
imranskylinx <imran.skylinxtech@gmail.com>
2023-08-23 13:40:40 +0500

    super admin now can see names of approver in grants

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,8 @@
                 <!-- Dashboard Ecommerce Starts -->
                 <section id="dashboard-ecommerce">
                     <div class="row">
-                        @if (auth()->user()->hasRole('Admin') ||auth()->user()->hasRole('Super Admin')  )
+                        @if (auth()->user()->hasRole('Admin') ||
+                                auth()->user()->hasRole('Super Admin'))
                             <div class="col-lg-3 col-sm-6 col-12">
                                 <div class="card">
                                     <div class="card-header d-flex flex-column align-items-start pb-0">
@@ -44,7 +45,8 @@
                             </div>
                         @endif
                         @if (auth()->user()->hasRole('Admin') ||
-                                auth()->user()->hasRole('Publisher') || auth()->user()->hasRole('Super Admin') )
+                                auth()->user()->hasRole('Publisher') ||
+                                auth()->user()->hasRole('Super Admin'))
                             <div class="col-lg-3 col-sm-6 col-12">
                                 <div class="card">
                                     <div class="card-header d-flex flex-column align-items-start pb-0">
@@ -55,7 +57,7 @@
                                         </div>
                                         <h2 class="text-bold-700 mt-1">
                                             {{ App\Models\Book::approved()->active()->when(
-                                                    !auth()->user()->hasRole('Admin'),
+                                                    !auth()->user()->hasRole('Super Admin'),
                                                     function ($query) {
                                                         $query->where('added_by', auth()->user()->id);
                                                     },
@@ -77,14 +79,14 @@
                                         </div>
                                         <h2 class="text-bold-700 mt-1">
                                             {{ App\Models\Book::pendingApprove()->when(
-                                                    !auth()->user()->hasRole('Admin'),
+                                                    !auth()->user()->hasRole('Super Admin'),
                                                     function ($query) {
                                                         $query->where('added_by', auth()->user()->id);
                                                     },
                                                 )->count() }}
                                         </h2>
                                         <p class="mb-0">Pending For Approval Book <u style="font-size: 10px">
-                                                @if (auth()->user()->hasRole('Admin'))
+                                                @if (auth()->user()->hasRole('Super Admin'))
                                                     <a href="{{ url('book/pending-for-approval') }}">Click</a>
                                                 @endif
                                             </u>
@@ -104,7 +106,7 @@
                                         </div>
                                         <h2 class="text-bold-700 mt-1">
                                             {{ App\Models\Book::rejected()->when(
-                                                    !auth()->user()->hasRole('Admin'),
+                                                    !auth()->user()->hasRole('Super Admin'),
                                                     function ($query) {
                                                         $query->where('added_by', auth()->user()->id);
                                                     },
@@ -129,7 +131,7 @@
                                         </div>
                                         <h2 class="text-bold-700 mt-1">
                                             {{ App\Models\Book::approved()->active()->ebook()->when(
-                                                    !auth()->user()->hasRole('Admin'),
+                                                    !auth()->user()->hasRole('Super Admin'),
                                                     function ($query) {
                                                         $query->where('added_by', auth()->user()->id);
                                                     },
@@ -154,7 +156,7 @@
                                         </div>
                                         <h2 class="text-bold-700 mt-1">
                                             {{ App\Models\Book::approved()->active()->audio()->when(
-                                                    !auth()->user()->hasRole('Admin'),
+                                                    !auth()->user()->hasRole('Super Admin'),
                                                     function ($query) {
                                                         $query->where('added_by', auth()->user()->id);
                                                     },
@@ -179,7 +181,7 @@
                                         </div>
                                         <h2 class="text-bold-700 mt-1">
                                             {{ App\Models\Book::approved()->active()->paper()->when(
-                                                    !auth()->user()->hasRole('Admin'),
+                                                    !auth()->user()->hasRole('Super Admin'),
                                                     function ($query) {
                                                         $query->where('added_by', auth()->user()->id);
                                                     },
@@ -195,7 +197,7 @@
                                 </div>
                             </div>
                         @endif
-                        @if (auth()->user()->hasRole('Institute') )
+                        @if (auth()->user()->hasRole('Institute'))
                             <div class="col-lg-3 col-sm-6 col-12">
                                 <div class="card">
                                     <div class="card-header d-flex flex-column align-items-start pb-0">
