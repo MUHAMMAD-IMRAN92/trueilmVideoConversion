2ffa3032b4772de6c85a17693f363321b9e5168b
imranskylinx <imran.skylinxtech@gmail.com>
2023-09-14 18:00:12 +0500

    optimizing code

--- app/Models/Surah.php
+++ app/Models/Surah.php
@@ -11,7 +11,7 @@ class Surah extends Eloquent
     use HasFactory;
     protected $connection = 'mongodb';
     protected $table = 'surahs';
-    // protected $appends = ['combination_translations'];
+    protected $appends = ['combination_translations'];
     public function ayats()
     {
         return $this->hasMany(AlQuran::class, 'surah_id', 'id');
