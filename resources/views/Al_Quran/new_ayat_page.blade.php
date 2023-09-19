eeb4478f34b94878ffd126068d9d05b798c627f0
imranskylinx <imran.skylinxtech@gmail.com>
2023-09-19 18:52:48 +0500

    Ui updates

--- resources/views/Al_Quran/new_ayat_page.blade.php
+++ resources/views/Al_Quran/new_ayat_page.blade.php
@@ -176,7 +176,7 @@
                                                         </div>
 
                                                         <div class="col-12 mt-2"
-                                                            style="@if ($currentCombination->language->title == 'Urdu' || $currentCombination->language->title == 'Arabic') display: flex;flex-direction: column;align-content: end;margin-right:7%; @endif">
+                                                            style="@if ($currentCombination->language->title == 'Urdu' || $currentCombination->language->title == 'Arabic') display: flex;flex-direction: column;align-content: end;margin-right:8%; @endif">
 
 
                                                             <input type="hidden" id="ayat-id-{{ $key }}"
@@ -190,7 +190,7 @@
                                                     </div>
                                                 </div>
                                                 <div class="row ml-1"
-                                                    style="@if ($currentCombination->language->title == 'Urdu' || $currentCombination->language->title == 'Arabic') display: flex;flex-direction: column;align-content: end;margin-right:7%; @endif">
+                                                    style="@if ($currentCombination->language->title == 'Urdu' || $currentCombination->language->title == 'Arabic') display: flex;flex-direction: column;align-content: end;margin-right:8%; @endif">
 
                                                     <span class="" id="non-edit-para-des-{{ $key }}"
                                                         style="margin-left:10px!important; ">
