eea0bfd0cc79386e3e4b0de40af7419933a2f329
imranskylinx <imran.skylinxtech@gmail.com>
2023-09-25 11:10:52 +0500

    search function

--- routes/api.php
+++ routes/api.php
@@ -29,4 +29,4 @@ Route::post('stripe/subscribe-plan', [App\Http\Controllers\Api\StripeController:
 
 
 Route::post('search',  [App\Http\Controllers\HomeController::class, 'search']);
-Route::post('search/index',  [App\Http\Controllers\HomeController::class, 'indexTranslation']);
+Route::get('search/index',  [App\Http\Controllers\HomeController::class, 'indexTranslation']);
