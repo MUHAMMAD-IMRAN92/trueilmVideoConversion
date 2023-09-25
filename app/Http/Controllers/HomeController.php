f4d845ff1fdd970f294e53595b94656493f87c46
imranskylinx <imran.skylinxtech@gmail.com>
2023-09-25 11:18:33 +0500

    search function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -334,13 +334,13 @@ class HomeController extends Controller
     {
         ini_set("memory_limit", "-1");
         $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
-        return $data = AlQuranTranslation::take(200)->get();
+        $data = AlQuranTranslation::take(200)->get()->toArray();
         $alQurantranslationsclient =  $client->index('alQurantranslations')->addDocuments($data, '_id');
 
 
 
 
-        return response()->json($data);
+        return response()->json($alQurantranslationsclient);
     }
 }
 
