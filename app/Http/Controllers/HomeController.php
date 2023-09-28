d8bcadd31c57e9bfbea21eb949b1eb69d314beeb
imranskylinx <imran.skylinxtech@gmail.com>
2023-09-28 11:52:00 +0500

    api updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -285,7 +285,7 @@ class HomeController extends Controller
         $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
         $arrIndex = [1 => 'ebooks', 2 => 'audio', 3 => 'papers', 4 => 'alQurantranslations', 5 => 'alHadeestranslations', 6 =>  'course', 7 => 'podcast', 8 => 'bookForSale', 9 => 'glossary'];
         $queries = [];
-        if ($request->type != "" || count($request->type) > 0) {
+        if ($request->type != "") {
             // $arr = explode(',', $request->type);
             foreach ($request->type as $ar) {
                 $queries[] = (new SearchQuery())
@@ -348,7 +348,7 @@ class HomeController extends Controller
 
     function searchTest(Request $request)
     {
-        return count($request->type);
+        // return ;
         ini_set("memory_limit", "-1");
         $validator = Validator::make($request->all(), [
             'type' => 'array',
@@ -359,7 +359,7 @@ class HomeController extends Controller
         $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
         $arrIndex = [1 => 'ebooks', 2 => 'audio', 3 => 'papers', 4 => 'alQurantranslations', 5 => 'alHadeestranslations', 6 =>  'course', 7 => 'podcast', 8 => 'bookForSale', 9 => 'glossary'];
         $queries = [];
-        if ($request->type != "") {
+        if ($request->type != "" || count($request->type) != 0) {
             // $arr = explode(',', $request->type);
             foreach ($request->type as $ar) {
                 $queries[] = (new SearchQuery())
