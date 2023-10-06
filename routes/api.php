3fe675ddafb7491df31c526dec4aad4f3141cbc7
imranskylinx <imran.skylinxtech@gmail.com>
2023-10-06 13:22:37 +0500

    api for testing

--- routes/api.php
+++ routes/api.php
@@ -38,13 +38,13 @@ Route::get('search/index',  [App\Http\Controllers\HomeController::class, 'indexT
 Route::get('/delete/index', function () {
     ini_set("memory_limit", "-1");
     $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
-    return  $book = Book::where('type', "1")->first();
-    // $book7= Book::where('type', "7")->get()->toArray();
+    $book = Book::where('type', "1")->first();
+    return $client->index('ebooks')->getDocument($book->_id, ['id', 'title']);
+
+    // $book7= Book::where('ebooks', "7")->get()->toArray();
 
     // $booksclient =  $client->index('podcast')->addDocuments($book7, '_id');
-    return 'ok';
+    // return 'ok';
 });
 
 Route::post('qr/generate',  [App\Http\Controllers\HomeController::class, 'generateQr']);
-
-$client->index('movies')->getDocument(25684, ['id', 'title', 'poster', 'release_date']);
