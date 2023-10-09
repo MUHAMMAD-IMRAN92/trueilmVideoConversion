fdacb59e3ab7427bb6a302783633b885147521e5
imranskylinx <imran.skylinxtech@gmail.com>
2023-10-09 13:39:17 +0500

    push notifications

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -229,3 +229,13 @@
     </div>
     <!-- END: Content-->
 @endsection
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+    OneSignalDeferred.push(function(OneSignal) {
+        OneSignal.init({
+            appId: "YOUR_ONESIGNAL_APP_ID",
+        });
+    });
+</script>
+
