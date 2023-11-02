f0700476a56f196ba7259327db19ce1b54628176
imranskylinx <imran.skylinxtech@gmail.com>
2023-11-02 19:13:59 +0500

    api update

--- app/Http/Controllers/Api/QuizController.php
+++ app/Http/Controllers/Api/QuizController.php
@@ -144,7 +144,7 @@ class QuizController extends Controller
             $attempt = new QuizAttempts();
             $attempt->user_id =  $request->user_id;
             $attempt->lesson_id = $request->lesson_id;
-            $attempt->start_date = Carbon::now('UTC');
+            $attempt->start_date = Carbon::now('UTC')->toDateTimeString();
             $attempt->is_ended = 0;
             $attempt->save();
 
