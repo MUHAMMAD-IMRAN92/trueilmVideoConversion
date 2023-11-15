c7bbc45e9e27a7e45e244a914608ca7a58cc8f14
imranskylinx <imran.skylinxtech@gmail.com>
2023-11-15 17:19:28 +0500

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -300,9 +300,11 @@ class HomeController extends Controller
             foreach ($res['results'] as $r) {
                 if ($r['indexUid'] == 'alHadeestranslations') {
                     foreach ($r['hits'] as $h) {
-                        // $Hadith = Hadees::where('_id',  $h['hadees_id'])->first();
+                        $Hadith = Hadees::where('_id',  $h['hadees_id'])->first();
 
-                        $h = [];
+                        if ($Hadith) {
+                            $h['Hadith'] = $Hadith;
+                        }
                     }
                 }
             }
