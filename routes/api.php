a2d59c3bca651316de0d633e40742bf4a4982357
imranskylinx <imran.skylinxtech@gmail.com>
2024-01-26 19:00:13 +0500

    indexing added

--- routes/api.php
+++ routes/api.php
@@ -58,7 +58,7 @@ Route::get('/delete/index', function () {
     $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
     $client->deleteIndex('ebook');
 
-    // $client->createIndex('audio', ['primaryKey' => '_id']);
+    $client->createIndex('ebook', ['primaryKey' => '_id']);
 
 
 
