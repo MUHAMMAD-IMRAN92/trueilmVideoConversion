5c1fe53083189667f08df928f64648cda5011194
imranskylinx <imran.skylinxtech@gmail.com>
2024-01-29 15:12:44 +0500

    test

--- routes/api.php
+++ routes/api.php
@@ -56,14 +56,14 @@ Route::get('search/index',  [App\Http\Controllers\HomeController::class, 'indexT
 Route::get('/delete/index', function () {
     ini_set("memory_limit", "-1");
     $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
-    $client->deleteIndex('ebook');
+    // $client->deleteIndex('ebook');
 
-    $client->createIndex('ebook', ['primaryKey' => '_id']);
+    // $client->createIndex('ebook', ['primaryKey' => '_id']);
 
 
 
-    $book = Book::where('type', '1')->where('approved', 1)->where('status', 1)->get()->toArray();
-    $client->index('ebook')->addDocuments($book);
+    $book = Book::where('type', '7')->where('approved', 1)->where('status', 1)->get()->toArray();
+    $client->index('podcast')->addDocuments($book);
     // return $client->index('ebooks')->getDocument($book->_id, ['id', 'title']);
 
     // $book7 = Book::where('type', "7")->get()->toArray();
