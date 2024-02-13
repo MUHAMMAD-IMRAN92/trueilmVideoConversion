6449f9a56a8e71a2ad18e2d28f1aa14e10a44c31
imranskylinx <imran.skylinxtech@gmail.com>
2024-02-13 10:44:40 +0500

    check

--- routes/web.php
+++ routes/web.php
@@ -379,6 +379,6 @@ Route::get('check', function () {
     set_time_limit(0);
     $alQuran = AlQuranTranslation::where('author_lang', "65ca0c95d5f8cfe031aeabea")->orderBy('created_at', 'DESC')->get()->take(700)->pluck('_id');
 
-    $alQuranTranslation =   AlQuranTranslation::whereIn('_id', $alQuran)->get();
+    $alQuranTranslation =   AlQuranTranslation::whereIn('_id', $alQuran)->delete();
     return $alQuranTranslation;
 });
