323166e5288d2e8f3371a803b7d4a0b46d345a87
imranskylinx <imran.skylinxtech@gmail.com>
2024-02-14 18:55:18 +0500

    session url

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -77,6 +77,7 @@ class StripeController extends Controller
                 $session = $stripe->billingPortal->sessions->create([
                     'customer' => $customer,
                     'return_url' =>   $request->return_url,
+                    'allow_promotion_codes' => true
                 ]);
                 return  sendSuccess('Billing Portal Url .', $session->url);
             } else {
@@ -93,7 +94,7 @@ class StripeController extends Controller
                     'customer' => $customer,
                     'success_url' =>  $request->success_url,
                     'cancel_url' =>  $request->cancel_url,
-                    'allow_promotion_codes' => true
+
                 ]);
                 $plan = Subscription::where('price_id', $request->price)->where('status', 0)->first();
                 $userSubscription = new UserSubscription();
