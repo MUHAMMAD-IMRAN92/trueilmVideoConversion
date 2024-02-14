7aa1ad2bcc9d16f6a64b78554e5d8e22b529d170
imranskylinx <imran.skylinxtech@gmail.com>
2024-02-14 19:17:10 +0500

    session url

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -90,7 +90,7 @@ class StripeController extends Controller
                         'quantity' => 1, // Set the quantity to 1 for a standard subscription
                     ]],
                     'mode' => 'subscription',
-                    'customer' => $customer,  'discounts' => [['coupon' => 'MYCODE']],
+                    'customer' => $customer,  'discounts' => [['coupon' => '{{tftJHSKO}}']],
                     'success_url' =>  $request->success_url,
                     'cancel_url' =>  $request->cancel_url,
 
