55f39ffec8a76b6a99a93bec06975573fb36a148
imranskylinx <imran.skylinxtech@gmail.com>
2024-02-19 18:33:27 +0500

    email response added

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -94,7 +94,7 @@ class UserController extends Controller
         // }
         if ($user) {
             try {
-              return  $api_key = env('MAIL_PASSWORD');
+                $api_key = env('MAIL_PASSWORD');
                 $api_url = "https://api.sendgrid.com/v3/mail/send";
 
                 // Set the email details and template variables
@@ -135,6 +135,7 @@ class UserController extends Controller
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 $response = curl_exec($ch);
                 curl_close($ch);
+                return sendSuccess('Mail Has Been Sent!', []);
             } catch (\Exception $e) {
                 return $e->getMessage();
             }
