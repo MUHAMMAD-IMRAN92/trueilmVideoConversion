7451a5f9d80f875ed75d03d3034657420dd6d92b
imranskylinx <imran.skylinxtech@gmail.com>
2024-03-08 13:09:36 +0500

    indexing for ALQuran

--- routes/api.php
+++ routes/api.php
@@ -106,6 +106,8 @@ Route::get('/course/index', function () {
         // }
         if ($key == 4) {
             $client->deleteIndex($arr);
+            $client->createIndex($arr, ['primaryKey' => 'id']);
+
             $book = AlQuranTranslation::where('author_lang', '65aa5d64c5da12cc4d009911')->where('type', 1)->chunk(1000, function ($AlQuran) use ($client, $arr) {
                 $client->index($arr)->addDocuments($AlQuran->toArray());
             });
