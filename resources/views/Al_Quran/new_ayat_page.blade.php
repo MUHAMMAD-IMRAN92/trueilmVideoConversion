76e2c4b1bf33fb1c7bb0a194877dcb2a5651f764
imranskylinx <imran.skylinxtech@gmail.com>
2024-03-18 16:35:48 +0500

    test

--- resources/views/Al_Quran/new_ayat_page.blade.php
+++ resources/views/Al_Quran/new_ayat_page.blade.php
@@ -476,17 +476,17 @@
 
                                                             <div class="" style="text-align: right;">:
                                                                 {{ $aya->sequence }} </div>
-                                                            @if (request()->khatoot != 2)
-                                                                {
-                                                                <span class="checkclass">{{ @$aya->arabic_aya_no }}</span>
-                                                                }
-                                                            @endif
+
                                                             <p id="non-edit-lang-select-{{ $key }}"
                                                                 class="mt-1 checkclassthird "
                                                                 style="text-align: right; line-height:50px;  @if (request()->khatoot == 2) direction:rtl; @endif">
                                                                 {!! str_replace('&#8203;', '', @$aya->khatoot[0]->ayat) !!}</p>
 
+                                                                @if (request()->khatoot != 2)
 
+                                                                <span class="checkclass">{{ @$aya->arabic_aya_no }}</span>
+
+                                                            @endif
                                                         </div>
 
                                                         <div class="col-12 mt-2"
