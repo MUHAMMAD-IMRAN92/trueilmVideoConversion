ce6a00a1596930d278c998e023bdf12cf77fac61
imranskylinx <imran.skylinxtech@gmail.com>
2024-03-18 16:34:12 +0500

    test

--- resources/views/Al_Quran/new_ayat_page.blade.php
+++ resources/views/Al_Quran/new_ayat_page.blade.php
@@ -476,18 +476,15 @@
 
                                                             <div class="" style="text-align: right;">:
                                                                 {{ $aya->sequence }} </div>
-
-                                                            <p id="non-edit-lang-select-{{ $key }}"
-                                                                class="mt-1 checkclassthird"
-                                                                style="text-align: right; line-height:50px">
-                                                                {!! str_replace('&#8203;', '', @$aya->khatoot[0]->ayat) !!}</p>
-                                                                @if(request()->khatoot != 2){
-                                                                     <span
-                                                                class="checkclass">{{ @$aya->arabic_aya_no }}</span>
+                                                            @if (request()->khatoot != 2)
+                                                                {
+                                                                <span class="checkclass">{{ @$aya->arabic_aya_no }}</span>
                                                                 }
-                                                                @endif
-
-
+                                                            @endif
+                                                            <p id="non-edit-lang-select-{{ $key }}"
+                                                                class="mt-1 checkclassthird "
+                                                                style="text-align: right; line-height:50px;  @if (request()->khatoot == 2) direction:rtl; @endif">
+                                                                {!! str_replace('&#8203;', '', @$aya->khatoot[0]->ayat) !!}</p>
 
 
                                                         </div>
