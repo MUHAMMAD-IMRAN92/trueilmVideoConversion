f9e2ee81ee1980da0433e8a01afa72bab0629a97
imranskylinx <imran.skylinxtech@gmail.com>
2024-03-20 11:57:23 +0500

    scheduler

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -387,8 +387,6 @@ class UserController extends Controller
     }
     public static function checkSubscriptionExpiry()
     {
-        \DB::table('test')->insert(['key' => 'value']);
-        return '0';
         $now = Carbon::now();
 
         $thirtyDaysAgo = $now->copy()->subDays(30);
@@ -399,44 +397,46 @@ class UserController extends Controller
         $api_key = env('MAIL_PASSWORD');
         $api_url = "https://api.sendgrid.com/v3/mail/send";
         foreach ($usersToEmail as $user) {
+            $user->update([
+                'istrail' => 0
+            ]);
+            $to_email =  'salam@trueilm.com';
+            $from_email = env('MAIL_FROM_ADDRESS');
+            $template_id = " d-8e1abcf085124ec9a9e5c356601f8f60";
+            $template_vars = [
+                'email' => $user->email
+            ];
 
-            // $to_email =  'salam@trueilm.com';
-            // $from_email = env('MAIL_FROM_ADDRESS');
-            // $template_id = "d-8f3cb730011d4d608f1ef9ab917d4a2e";
-            // $template_vars = [
-            //     'email' => $user->email
-            // ];
+            // Set the payload as a JSON string
+            $payload = json_encode([
+                "personalizations" => [
+                    [
+                        "to" => [
+                            [
+                                "email" => $to_email
+                            ]
+                        ],
+                        "dynamic_template_data" => $template_vars
+                    ]
+                ],
+                "from" => [
+                    "email" => $from_email
+                ],
+                "template_id" => $template_id
+            ]);
 
-            // // Set the payload as a JSON string
-            // $payload = json_encode([
-            //     "personalizations" => [
-            //         [
-            //             "to" => [
-            //                 [
-            //                     "email" => $to_email
-            //                 ]
-            //             ],
-            //             "dynamic_template_data" => $template_vars
-            //         ]
-            //     ],
-            //     "from" => [
-            //         "email" => $from_email
-            //     ],
-            //     "template_id" => $template_id
-            // ]);
-
-            // // Set the cURL options and send the POST request
-            // $ch = curl_init();
-            // curl_setopt($ch, CURLOPT_URL, $api_url);
-            // curl_setopt($ch, CURLOPT_POST, 1);
-            // curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-            // curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            //     "Authorization: Bearer $api_key",
-            //     "Content-Type: application/json"
-            // ]);
-            // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            // $response = curl_exec($ch);
-            // curl_close($ch);
+            // Set the cURL options and send the POST request
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $api_url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                "Authorization: Bearer $api_key",
+                "Content-Type: application/json"
+            ]);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            curl_close($ch);
         }
 
         return '0';
