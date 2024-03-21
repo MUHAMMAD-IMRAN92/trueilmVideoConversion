7e9b24e44941cfa782cc234d8b2aec55d1f8db12
imranskylinx <imran.skylinxtech@gmail.com>
2024-03-21 14:20:58 +0500

    cron job method changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->call(
-            UserController::checkSubscriptionExpiry()
+            [new UserController(), 'checkSubscriptionExpiry']
         )->dailyAt('13:00');
     }
 
