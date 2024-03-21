291f07a5f1f7ebb419ec7a3566b5019617648ab0
imranskylinx <imran.skylinxtech@gmail.com>
2024-03-21 11:38:23 +0500

    languages order by title

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -25,7 +25,7 @@ class LanguageController extends Controller
     public function index()
     {
 
-        $languages =  Languages::orderBy('title', 'ASC')->all();
+        $languages =  Languages::orderBy('title', 'ASC')->get();
 
         return view('language.index', [
             'languages' =>  $languages
