245d9679e7e7412661e4a5949ac619adb12596a2
imranskylinx <imran.skylinxtech@gmail.com>
2024-03-22 11:13:30 +0500

    new translation from Quranenc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -727,9 +727,9 @@ class HomeController extends Controller
     {
         ini_set('max_execution_time', 0);
         ini_set("memory_limit", "-1");
-        $lang = '65fd10a56e907be5274e28f5';
+        $lang = '654c78ba7eae546fedb1fbba';
         $authors = [
-            'asante_harun' => "65fd10b86e907be5274e28f6"
+            'azeri_musayev' => "65fd214a6e907be5274e28f7"
         ];
         $records = [];
         foreach ($authors  as $nokey => $arr) {
