dc12be4a62e4b6a1b84113c12790bd244a164522
imranskylinx <imran.skylinxtech@gmail.com>
2024-04-01 12:45:35 +0500

    ffmpeg video conversion

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -29,30 +29,23 @@ class DevController extends Controller
     }
     public function post(Request $request)
     {
-        $inputFile = $request->file;
-        $outputDir = 'public/';
+        $config = [
+            'ffmpeg.binaries'  => '/usr/bin/ffmpeg',
+            'ffprobe.binaries' => '/usr/bin/ffprobe',
+            'timeout'          => 3600, // The timeout for the underlying process
+            'ffmpeg.threads'   => 12,   // The number of threads that FFmpeg should use
+        ];
 
+        $log = new Logger('FFmpeg_Streaming');
+        $log->pushHandler(new StreamHandler('/var/log/ffmpeg-streaming.log')); // path to log file
 
-        // Create a directory for HLS segments
-        $outputDir .= 'output.m3u8';
-
-        // Execute FFmpeg command
-        $process = new Process([
-            'ffmpeg',
-            '-i', $inputFile,
-            '-vf', 'scale=-2:480', // Adjust resolution if needed
-            '-c:a', 'aac',
-            '-c:v', 'h264',
-            '-hls_time', '10', // Segment duration in seconds
-            '-hls_list_size', '0', // List all segments in playlist
-            $outputDir
-        ]);
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException($process->getErrorOutput());
-        }
-
-        dd($process);
+        $ffmpeg = \Streaming\FFMpeg::create($config, $log);
+        $video = $ffmpeg->open($request->file);
+        $video->dash()
+            ->x264() // Format of the video. Alternatives: hevc() and vp9()
+            ->autoGenerateRepresentations() // Auto generate representations
+            ->save(); // It can be passed a path to the method or it can be null
+        return $video;
         // $config = [
         //     'ffmpeg.binaries'  => 'C:\ffmpeg\ffmpeg-master-latest-linux64-gpl\bin',
         //     'ffprobe.binaries' => 'C:\ffmpeg\ffmpeg-master-latest-linux64-gpl\bin',
