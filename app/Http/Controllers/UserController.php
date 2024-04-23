f2661b9d33913a4373c8f51c0d8b24338e154201
imranskylinx <imran.skylinxtech@gmail.com>
2024-04-23 11:03:06 +0500

    plans for institute

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -297,7 +297,47 @@ class UserController extends Controller
         $user->institute_id =  $this->user->id;
         $user->user_id =  null;
         $user->save();
+        $api_key = env('MAIL_PASSWORD');
+        $api_url = "https://api.sendgrid.com/v3/mail/send";
 
+        // Set the email details and template variables
+        $to_email =  $user->email;
+        $from_email = env('MAIL_FROM_ADDRESS');
+        $template_id = "d-2fa42cbbaef44184977f05de22616359";
+        $template_vars = [
+            'parentName' => auth()->user()->name,
+        ];
+
+        // Set the payload as a JSON string
+        $payload = json_encode([
+            "personalizations" => [
+                [
+                    "to" => [
+                        [
+                            "email" => $to_email
+                        ]
+                    ],
+                    "dynamic_template_data" => $template_vars
+                ],
+            ],
+            "from" => [
+                "email" => $from_email
+            ],
+            "template_id" => $template_id
+        ]);
+
+        // Set the cURL options and send the POST request
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "Authorization: Bearer $api_key",
+            "Content-Type: application/json"
+        ]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
         return redirect()->to('/institute/users')->with('msg', 'User Saved Successfully!');
     }
     public function deleteInstituteUsers($id)
