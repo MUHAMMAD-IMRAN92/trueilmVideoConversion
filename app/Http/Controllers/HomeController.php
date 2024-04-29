a581e6e1cae3b40a18b592d0dda04ad91c34d0be
imranskylinx <imran.skylinxtech@gmail.com>
2024-04-29 17:40:51 +0500

    new Translation Quranenc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -707,7 +707,7 @@ class HomeController extends Controller
         ini_set("memory_limit", "-1");
         $lang = '6628f34b3b5ca447c07fb3fd';
         $authors = [
-            'uzbek_rwwad' => "6548c59c80681501aab8029b",
+            'uzbek_mansour' => "662f95293b5ca447c07fb421",
         ];
         $records = [];
         foreach ($authors  as $nokey => $arr) {
