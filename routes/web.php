76d554153da507bca004d66fad3442888c6037d5
imranskylinx <imran.skylinxtech@gmail.com>
2024-06-25 16:33:12 +0500

    indexing

--- routes/web.php
+++ routes/web.php
@@ -458,7 +458,7 @@ Route::get('/indexing', function () {
     $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
 
     $client->createIndex('alHadeestranslations');
-    $books = HadeesBooks::where('_id', '655f47441c3df94998007a1a')->first();
+    $books = HadeesBooks::where('_id', '65ce0923e08adee9f4146f27')->first();
     if ($books) {
         // Fetch the translations for the book
         $data = HadeesTranslation::where('book_id', $books->_id)->get()->map(function ($tran) {
