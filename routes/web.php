002c9449b5d43fe4c73a0c8e618e6acf985a6557
imranskylinx <imran.skylinxtech@gmail.com>
2024-06-25 16:04:29 +0500

    indexing

--- routes/web.php
+++ routes/web.php
@@ -460,17 +460,16 @@ Route::get('/indexing', function () {
     $client = new  Client('http://localhost:7700', '3bc7ba18215601c4de218ef53f0f90e830a7f144');
 
     $client->createIndex('alHadeestranslations');
-    $books = HadeesBooks::get();
-    foreach ($books as $book) {
-        HadeesTranslation::where('book_id', $book->_id)->chunk(10, function ($translations) use ($client) {
-            $data = $translations->map(function ($tran) {
-                $tran->main_chapter = $tran->mainChapter();
-                return $tran;
-            });
+    $books = HadeesBooks::first();
 
-            $client->index('alHadeestranslations')->addDocuments($data->toArray(), '_id');
+    HadeesTranslation::where('book_id', $books->_id)->chunk(10, function ($translations) use ($client) {
+        $data = $translations->map(function ($tran) {
+            $tran->main_chapter = $tran->mainChapter();
+            return $tran;
         });
-    }
+
+        $client->index('alHadeestranslations')->addDocuments($data->toArray(), '_id');
+    });
 
     return 'ok';
 });
