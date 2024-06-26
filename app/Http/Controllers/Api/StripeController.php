93f0a0edfe11496ea10e5156a724aa252f5ef023
imranskylinx <imran.skylinxtech@gmail.com>
2024-06-26 16:06:53 +0500

    stripe discount

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -257,6 +257,7 @@ class StripeController extends Controller
 
     public function sessionUrlTest(Request $request)
     {
+        return 'imran';
         $user = User::where('email', $request->email)->first();
         if ($user) {
             $customer =   $user->customer;
