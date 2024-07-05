3c67ca7570559b20ea702c44851a4e5d21a15ede
khuramnaqvi5 <138566144+khuramnaqvi5@users.noreply.github.com>
2024-07-05 16:03:12 +0500

    roles and permiossion

--- routes/web.php
+++ routes/web.php
@@ -452,6 +452,9 @@ Route::get('phpinfo', function () {
 Route::get('dev', function () {
     // \File::makeDirectory(public_path('videos'), 777, true, true);
     // \File::deleteDirectory(public_path('videos'), 777, true, true);
+
+   
+
     system("rm -rf ".escapeshellarg(public_path('videos')));
     return 'ok';
     $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
