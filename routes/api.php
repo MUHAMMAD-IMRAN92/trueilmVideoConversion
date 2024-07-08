cd83fb6db4af547e89a3fa9a982f94b85279ee7f
imranskylinx <imran.skylinxtech@gmail.com>
2024-07-08 19:27:41 +0500

    api routes changes for indexing

--- routes/api.php
+++ routes/api.php
@@ -73,16 +73,13 @@ Route::get('/quran/index/{id}', function ($id) {
     $authorLang = AuthorLanguage::where('type', 1)->get();
     foreach ($authorLang as $authlang) {
 
-        AlQuranTranslation::where('author_lang', $authlang->_id)->chunk(100, function ($translations) use ($client) {
-            $data = $translations->map(function ($tran) {
-                $tran->lang_id = $tran->language();
-                $tran->author_id = $tran->author();
+        $translation =   AlQuranTranslation::where('author_lang', $authlang->_id)->map(function ($tran) {
+            $tran->lang_id = $tran->language();
+            $tran->author_id = $tran->author();
 
-                return $tran;
-            });
-
-            $client->index('alQurantranslations')->addDocuments($data->toArray(), '_id');
+            return $tran;
         });
+        $client->index('alQurantranslations')->addDocuments($translation->toArray(), '_id');
     }
     return 'ok';
     // ini_set("memory_limit", "-1");
